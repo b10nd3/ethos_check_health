957e9fc401417fab79552f56203208bce7e4713a
b10nd3 <b10nd3@mail.ru>
2019-09-06 08:28:19 +0300

    add GPU count variable

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ethos_check_health
-Check card status in Ethos
+Check card status in Ethos. 
+Put this file in /home/ethos directory and add string
+
+*/10 * * * * /home/ethos/ethos_check_health.sh
+
+in crontab.
--- ethos_check_health.sh
+++ ethos_check_health.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+Gpu_count=6 #Enter Your number of GPUs
+
 str=`cat /var/run/ethos/stats.file | grep -i '^mem:' `
 IFS=' ' # space is set as delimiter
 read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
 for i in "${ADDR[@]}";
 do # access each element of array
         if [ "$i" == "300" ]; then
-		sudo /sbin/reboot
+                sudo /sbin/reboot
 #echo "$i"
 fi
 done
 
 
 read var1 < /var/run/ethos/gpucount.file
-if [ "$var1" = "8" ]; then
-	echo $var1 adapters
+if [ "$var1" == "$Gpu_count" ]; then
+        echo $var1 adapters
 else
-	sudo /sbin/reboot
+        sudo /sbin/reboot
 fi
 
 exit 0
