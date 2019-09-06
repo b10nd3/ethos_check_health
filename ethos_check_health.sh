75752526fd40cab11e7296cdad1face930be5631
b10nd3 <b10nd3@mail.ru>
2019-09-06 08:15:47 +0300

    my first file in first git project

---
+++ ethos_check_health.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+str=`cat /var/run/ethos/stats.file | grep -i '^mem:' `
+IFS=' ' # space is set as delimiter
+read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
+for i in "${ADDR[@]}";
+do # access each element of array
+        if [ "$i" == "300" ]; then
+		sudo /sbin/reboot
+#echo "$i"
+fi
+done
+
+
+read var1 < /var/run/ethos/gpucount.file
+if [ "$var1" = "8" ]; then
+	echo $var1 adapters
+else
+	sudo /sbin/reboot
+fi
+
+exit 0
